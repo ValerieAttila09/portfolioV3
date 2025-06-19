e9287a9435824b5fa3fae0f69c1c4d42c6d4fe2f
ValerieAttila09 <akunvalerie76@gmail.com>
2025-06-19 15:05:36 +0700

    commited

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,9 @@ import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(useGSAP)
 
-const handleAnimationComplete = () => {
-	console.log('All letters have animated!');
-};
-
 const Text = () => {
 	return (
-		<h1 className='text-5xl pt-sans-bold text-start'>WEB DESIGNER & DEVELOPER</h1>
+		<h1 className='text-6xl pt-sans-bold text-start'>WEB DESIGNER & DEVELOPER</h1>
 	)
 }
 
@@ -33,7 +29,7 @@ export function Header() {
 	return (
 		<>
 			<div className='md:my-[9rem] lg:flex lg:gap-[6rem] lg:my-10'>
-				<div className="w-full h-[26rem] md:h-[36rem] flex flex-col gfap-5 p-4">
+				<div className="w-full h-[28rem] md:h-[36rem] flex flex-col gap-5 p-4">
 					<div className="w-full h-1/2 flex items-end">
 						<div className="flex flex-col items-start justify-center gap-2">
 							<SplitText 
@@ -45,7 +41,6 @@ export function Header() {
 								to={{ opacity: 1, y: 0 }} 
 								threshold={0.05} 
 								rootMargin="-100px" 
-								onLetterAnimationComplete={handleAnimationComplete} 
 							/>
 						</div>
 					</div>
@@ -58,7 +53,7 @@ export function Header() {
 					</div>
 				</div>
 
-				<div className='bili md:my-10 w-full h-[14rem] md:h-[20rem] flex justify-center items-center'>
+				<div className='bili md:my-6 w-full h-[14rem] md:h-[16rem] flex justify-center items-center'>
 					<div className="w-full relative h-auto">
 						<div className="absolute max-w-md flex flex-col inset-x-5 md:inset-x-0 shadow-xl hover:-translate-y-[1rem] hover:shadow-2xl transition-all">
 							<div className="w-full rounded-tr-md rounded-tl-md bg-neutral-800 flex justify-between">
@@ -88,13 +83,13 @@ export function Header() {
 											<br />
 											<span className='text-sm text-nowrap text-light'><code className='text-yellow-400'>{`{`}</code></span>
 											<br />
-											<span className='text-sm text-nowrap text-light pl-4'><code><span className='text-green-400'>"name"</span>: <span className='text-teal-500'><SplitText text={"\"Valerie\""} duration={0.6} ease="power3.out" splitType="chars" from={{ opacity: 0, y: 10 }} to={{ opacity: 1, y: 0 }} threshold={0.1} rootMargin="-100px" textAlign="center" onLetterAnimationComplete={handleAnimationComplete} /></span>,</code></span>
+											<span className='text-sm text-nowrap text-light pl-4'><code><span className='text-green-400'>"name"</span>: <span className='text-teal-500'><SplitText text={"\"Valerie\""} duration={0.6} ease="power3.out" splitType="chars" from={{ opacity: 0, y: 10 }} to={{ opacity: 1, y: 0 }} threshold={0.1} rootMargin="-100px" textAlign="center" /></span>,</code></span>
 											<br />
-											<span className='text-sm text-nowrap text-light pl-4'><code><span className="text-green-400">"major"</span>: <span className="text-teal-500"><SplitText text={"\"Software Engineering\""} duration={0.6} ease="power3.out" splitType="chars" from={{ opacity: 0, y: 10 }} to={{ opacity: 1, y: 0 }} threshold={0.1} rootMargin="-100px" textAlign="center" onLetterAnimationComplete={handleAnimationComplete} /></span>,</code></span>
+											<span className='text-sm text-nowrap text-light pl-4'><code><span className="text-green-400">"major"</span>: <span className="text-teal-500"><SplitText text={"\"Software Engineering\""} duration={0.6} ease="power3.out" splitType="chars" from={{ opacity: 0, y: 10 }} to={{ opacity: 1, y: 0 }} threshold={0.1} rootMargin="-100px" textAlign="center" /></span>,</code></span>
 											<br />
-											<span className='text-sm text-nowrap text-light pl-4'><code><span className="text-green-400">"profession"</span>: <span className="text-teal-500"><SplitText text={"\"Web Designer & Developer\""} duration={0.6} ease="power3.out" splitType="chars" from={{ opacity: 0, y: 10 }} to={{ opacity: 1, y: 0 }} threshold={0.1} rootMargin="-100px" textAlign="center" onLetterAnimationComplete={handleAnimationComplete} /></span>,</code></span>
+											<span className='text-sm text-nowrap text-light pl-4'><code><span className="text-green-400">"profession"</span>: <span className="text-teal-500"><SplitText text={"\"Web Designer & Developer\""} duration={0.6} ease="power3.out" splitType="chars" from={{ opacity: 0, y: 10 }} to={{ opacity: 1, y: 0 }} threshold={0.1} rootMargin="-100px" textAlign="center" /></span>,</code></span>
 											<br />
-											<span className='text-sm text-nowrap text-light pl-4'><code><span className='text-green-400'>"skills"</span>: <span className='text-yellow-500'>[</span><span className='text-teal-500'><SplitText text={"\"HTML\""} duration={0.6} ease="power3.out" splitType="chars" from={{ opacity: 0, y: 10 }} to={{ opacity: 1, y: 0 }} threshold={0.1} rootMargin="-100px" textAlign="center" onLetterAnimationComplete={handleAnimationComplete} /></span>, <span className='text-teal-500'><SplitText text={"\"Tailwind CSS\""} duration={0.6} ease="power3.out" splitType="chars" from={{ opacity: 0, y: 10 }} to={{ opacity: 1, y: 0 }} threshold={0.1} rootMargin="-100px" textAlign="center" onLetterAnimationComplete={handleAnimationComplete} /></span>, <span className='text-teal-500'><SplitText text={"\"JS\""} duration={0.6} ease="power3.out" splitType="chars" from={{ opacity: 0, y: 10 }} to={{ opacity: 1, y: 0 }} threshold={0.1} rootMargin="-100px" textAlign="center" onLetterAnimationComplete={handleAnimationComplete} /></span><span className='text-yellow-500'>]</span></code></span>
+											<span className='text-sm text-nowrap text-light pl-4'><code><span className='text-green-400'>"skills"</span>: <span className='text-yellow-500'>[</span><span className='text-teal-500'><SplitText text={"\"HTML\""} duration={0.6} ease="power3.out" splitType="chars" from={{ opacity: 0, y: 10 }} to={{ opacity: 1, y: 0 }} threshold={0.1} rootMargin="-100px" textAlign="center" /></span>, <span className='text-teal-500'><SplitText text={"\"Tailwind CSS\""} duration={0.6} ease="power3.out" splitType="chars" from={{ opacity: 0, y: 10 }} to={{ opacity: 1, y: 0 }} threshold={0.1} rootMargin="-100px" textAlign="center" /></span>, <span className='text-teal-500'><SplitText text={"\"JS\""} duration={0.6} ease="power3.out" splitType="chars" from={{ opacity: 0, y: 10 }} to={{ opacity: 1, y: 0 }} threshold={0.1} rootMargin="-100px" textAlign="center" /></span><span className='text-yellow-500'>]</span></code></span>
 											<br />
 											<span className='text-sm text-nowrap text-light'><code className='text-yellow-400'>{`}`}</code></span>
 										</div>
@@ -105,6 +100,7 @@ export function Header() {
 					</div>
 				</div>
 			</div>
+
 			<div className="bili w-full my-[12rem] md:my-[6rem]">
 				<div className="w-full flex justify-center items-center p-4">
 					<h1 className='text-3xl text-dark'>Made with</h1>
@@ -112,7 +108,7 @@ export function Header() {
 				<div className="w-full flex flex-wrap justify-center items-center gap-[1.7rem] p-4">
 					{Madewith.map((made) => (
 						<div className='flex items-center gap-3'>
-							<img key={made.id} src={made.image} className='h-10 w-auto' alt="" />
+							<img src={made.image} className='h-10 w-auto' alt="" />
 							<span className={made.classes}>{made.name}</span>
 						</div>
 					))}
